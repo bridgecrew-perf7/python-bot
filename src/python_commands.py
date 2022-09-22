4fd2f7b86844a9ed99ff7b580c0ab7a91b3bd958
julian-salinas <juliansalinasm@gmail.com>
2022-09-22 18:55:45 -0300

    Fix: debug issue

--- src/python_commands.py
+++ src/python_commands.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import re
-import discord
 from inplace import protected_inplacevar
 from discord.ext import commands
 import multiprocess
@@ -20,7 +19,7 @@ class PythonCog(commands.Cog):
         message = re.sub("|\```python|\`|import os|```py","",message)
 
         pool = ProcessPool(nodes = 4) 
-        debug = pool.apipe(self.interpret,message)
+        debug = pool.apipe(interpret,message)
 
         try: # 7 seconds timeout predefined
             output = debug.get(timeout = 7)
@@ -34,68 +33,68 @@ class PythonCog(commands.Cog):
         await ctx.send(output)
 
 
-    def interpret(self, code):
-        #  Necessary to show output, save in data ['results'] the prints of the execution
-        code += "\nresults = printed" 
+def interpret(code):
+    #  Necessary to show output, save in data ['results'] the prints of the execution
+    code += "\nresults = printed" 
 
-        #  Interpreter, mode exec to be able to use statements, methods, classes, functions, etc.
-        byte_code = compile_restricted(
-            code,
-            filename = "<string>",
-            mode = "exec",
-        )
-        #  All the functionalities available to the bot are stored in data, mostly builtins
-        data = {
-            "_print_"      : PrintCollector,
-            "__builtins__" : {
-                **utility_builtins,
-                "__import__"            : __import__,
-                "_inplacevar_"          : protected_inplacevar,
-                "all"                   : all,
-                "any"                   : any,
-                "_getiter_"             : RestrictedPython.Eval.default_guarded_getiter,        # iteraciones en bucles
-                "_getitem_"             : RestrictedPython.Eval.default_guarded_getitem,        # acceder a un valor de una lista por su indice
-                "_unpack_sequence_"     : RestrictedPython.Guards.guarded_iter_unpack_sequence, 
-                "_iter_unpack_sequence_": RestrictedPython.Guards.guarded_iter_unpack_sequence,
-                # Las ultimas 2 keys (lineas 65 y 66) son básicamente lo mismo, pero es necesario 
-                # que ambas estén declaradas para que el interprete las use,
-                # tienen funciones que operan con listas y strings, como por ejemplo, split
-                "round"                 : round,
-                "len"                   : len,
-                "list"                  : list,
-                "range"                 : range,
-                "abs"                   : abs,
-                "dict"                  : dict,
-                "str"                   : str,
-                "int"                   : int,
-                "max"                   : max,
-                "round"                 : round,
-                "map"                   : map,
-                "ascii"                 : ascii,
-                "bool"                  : bool,
-                "bytearray"             : bytearray,
-                "bytes"                 : bytes,
-                "eval"                  : eval,
-                "exec"                  : exec,
-                "filter"                : filter,
-                "float"                 : float,
-                "format"                : format,
-                "hash"                  : hash,
-                "id"                    : id,
-                "iter"                  : iter,
-                "locals"                : locals,
-                "min"                   : min,
-                "ord"                   : ord,
-                "pow"                   : pow,
-                "repr"                  : repr,
-                "reversed"              : reversed,
-                "sum"                   : sum,
-                "set"                   : set,
-                "sorted"                : sorted
+    #  Interpreter, mode exec to be able to use statements, methods, classes, functions, etc.
+    byte_code = compile_restricted(
+        code,
+        filename = "<string>",
+        mode = "exec",
+    )
+    #  All the functionalities available to the bot are stored in data, mostly builtins
+    data = {
+        "_print_"      : PrintCollector,
+        "getattr"      : getattr,
+        "__builtins__" : {
+            **utility_builtins,
+            "__import__"            : __import__,
+            "_inplacevar_"          : protected_inplacevar,
+            "all"                   : all,
+            "any"                   : any,
+            "_getiter_"             : RestrictedPython.Eval.default_guarded_getiter,        # iteraciones en bucles
+            "_getitem_"             : RestrictedPython.Eval.default_guarded_getitem,        # acceder a un valor de una lista por su indice
+            "_getattr_"               : getattr,
+            "_unpack_sequence_"     : RestrictedPython.Guards.guarded_iter_unpack_sequence, 
+            "_iter_unpack_sequence_": RestrictedPython.Guards.guarded_iter_unpack_sequence,
+            "slice"                 : slice,
+            "round"                 : round,
+            "len"                   : len,
+            "list"                  : list,
+            "range"                 : range,
+            "abs"                   : abs,
+            "dict"                  : dict,
+            "str"                   : str,
+            "int"                   : int,
+            "max"                   : max,
+            "round"                 : round,
+            "map"                   : map,
+            "ascii"                 : ascii,
+            "bool"                  : bool,
+            "bytearray"             : bytearray,
+            "bytes"                 : bytes,
+            "eval"                  : eval,
+            "exec"                  : exec,
+            "filter"                : filter,
+            "float"                 : float,
+            "format"                : format,
+            "hash"                  : hash,
+            "id"                    : id,
+            "iter"                  : iter,
+            "locals"                : locals,
+            "min"                   : min,
+            "ord"                   : ord,
+            "pow"                   : pow,
+            "repr"                  : repr,
+            "reversed"              : reversed,
+            "sum"                   : sum,
+            "set"                   : set,
+            "sorted"                : sorted,
+            "next"                  : next,
+        },
+        "_getattr_": RestrictedPython.Guards.safer_getattr
+    }
 
-            },
-            "_getattr_": RestrictedPython.Guards.safer_getattr
-        }
-        
-        exec(byte_code, data, None) #  Get the output
-        return data["results"] #  data["results"] only contains prints or NameErrors if exists
+    exec(byte_code, data, None) #  Get the output
+    return data["results"] #  data["results"] only contains prints or NameErrors if exists
